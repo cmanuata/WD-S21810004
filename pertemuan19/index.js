fc8b8e0f5f760cba316c61c6397ae602b1b994dc
cmanuata <cmanuata@gmail.com>
2020-07-05 23:28:50 +0700

    menambahkan pertemuan19

---
+++ pertemuan19/index.js
@@ -0,0 +1,68 @@
+//JavaScript Conditional
+        
+        /* Satu kondisi
+            console.log("JavaScript Conditional..");
+
+            let firstName = 'Jhon';
+            let isMarried = true;
+
+            if (!isMarried){
+                  console.log(firstName + "Will hopefully Married");} 
+            else{
+                  console.log(firstName + " is Married"); }
+        */
+
+        /*  dua kondisi
+            let nilaiAndi = 95;
+       
+             if(nilaiAndi>80){
+                console.log("Sangat Memuaskan");
+                }
+            else if(nilaiAndi >=60 && nilaiAndi <=80){
+                 console.log("Memuaskan");
+                }
+            else{
+                console.log("Jangan Menyerah! Coba lagi!");
+            } 
+        */
+
+            //SWITCH CASE
+    /*
+    let job = 'instructor';
+     let job = 'driver';
+     let job = 'designer';
+     let firstName = 'jhon';
+
+     switch(job)
+     {
+         case "instructor":
+             console.log(firstName + "is instructor");
+             break;
+        case "driver":
+            console.log(firstName + " is driver");
+            break;
+        case "designer":
+            console.log(firstName + "is designer");
+            break;
+        default:
+            console.log(firstName + "something else");
+            break;
+     }
+     */
+
+//fungsi 
+     //global
+        let firtName= 'Jhon';
+
+     function calculateAge(birthYear){
+     let age = 2020- birthYear;
+     console.log(firtName + " age is" + age);
+     }
+
+     console.log(firtName);
+     console.log(age); 
+    
+     //const calculateAge = function (birthYear){
+    //  return 2020-birthYear; }
+     //let age = calculateAge(1991);
+
