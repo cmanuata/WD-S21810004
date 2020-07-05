744059ca026d3895527c87d3164e081d5e4d1d73
cmanuata <cmanuata@gmail.com>
2020-07-05 23:26:48 +0700

    menambahkan pertemuan18

---
+++ pertemuan18/index.js
@@ -0,0 +1,95 @@
+
+// 1. variable dan tipe data 
+/*console.log("Hello World dari External JS");
+
+    let firstName = "John";
+    let $number = 3;
+    let years;
+    var age = 23;
+    let isMarried = false;
+
+    firstName = "Jane";
+
+    console.log(firstName);
+    console.log($number);
+    console.log(isMarried);
+    console.log(years); */
+
+//operator aritmetika
+
+/*let num1 =10;
+let num2 = 6;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(num1 ** num2);
+num1++;
+console.log(num1);
+num1--;
+console.log(num1); */
+
+//Assigment Operator
+ 
+/*let num3 =10;
+let num4;
+
+num4 = num3;
+console.log(num4);
+
+num3+=2;
+console.log(num3);
+
+num3 **=2;
+console.log(num3); */
+
+//string Operator
+
+/*let firstName = "Jhon";
+let lastName = "Doe";
+let age= 25;
+
+console.log(firstName +" " + lastName);
+console.log(firstName+age); */
+
+//Operator Perbandingan
+
+/*let num1 = 10;
+
+console.log(num1 == 10);
+console.log(num1 == "10");
+
+console.log(num1===10);
+console.log(num1=== "10");
+
+console.log(num1 >= 10);
+console.log(num1 <= 10);
+
+let num2 = num1 === 10 ? "Ten" : "Another Number";
+console.log(num2);
+*/
+
+//Operator Logika
+/*let hasCertificate = true;
+let hasTranscript = false;
+
+console.log(hasCertificate && hasTranscript); 
+console.log(!hasTranscript); */
+
+//Operator Presedence
+/*let result = 10 + 2 * 3; 
+
+console.log(result); */
+
+let massJohn=60; 
+let massMark=85; 
+let heightJohn=1.7; 
+let heightMark=1.8; 
+
+let BmiJohn=massJohn/(heightJohn*heightJohn); 
+let BmiMark=massMark/(heightMark*heightMark); 
+
+let isHigher=Bmiark>BmiJohn; 
+console.log("Is Mark's BMI higher than John's? "+isHigher);
